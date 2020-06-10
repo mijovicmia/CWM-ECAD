1e270ef3f9b9f3599f91cc56fe36271c390725da
Cloud User <centos@ip-172-31-10-114.eu-west-2.compute.internal>
2020-06-10 14:44:19 +0000

    In progress

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name: Mia Mijovic
+// Date: 10/06/2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -12,4 +12,35 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module traffic_lights(
+	input clk,
+	output reg red,
+	output reg amber,
+	output reg green
+	);
+
+	always @(posedge clk) begin
+		case ({red, amber, green})
+		3'b100 : begin 
+			red = 1;
+			amber = 1;
+			green = 0;
+			end
+		3'b110 : begin 
+			red = 0;
+			amber = 0;
+			green = 1;
+			end
+		3'b001 : begin 
+			red = 0;
+			amber = 1;
+			green = 0;
+			end
+		endcase
+	end
+endmodule
+
 
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,71 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name: Mia Mijovic
+// Date: 10/06/2020
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+
+    //Parameters
+	parameter CLK_PERIOD = 10;
+    //Registers and wires
+	reg clk;
+	wire [2:0] light;
+	reg [2:0] light_prev;
+	reg err;
+
+	// light[2:0] = [red amber green]
+
+	initial begin
+	clk = 1'b0;
+	forever
+		#(CLK_PERIOD/2) clk=~clk;
+	end
+	end
+
+	initial begin
+	err = 0;
+	light_prev = light;
+	clk = 0;
+	forever begin
+	#CLK_PERIOD
+	if ((light != 3'b100)&&(light != 3'b110)&&(light != 3'b001)) begin
+		err = 1;
+		$display("*** Test Failed :( *** light = %b", light);
+	end
+	else
+	if (((light == 3'b110)&&(light_prev != 3'b100))||((light == 3'b001)&&(light_prev != 3'b110))||((light == 3'b010)&&(light_prev != 3'b001))) begin
+		err = 1;
+		$display("*** Test Failed :( *** light = %b and light_prev = %b", light, light_prev);	
+
+	end
+	light_prev = light;
+	end
+end
+
+      	initial begin
+        	#500
+        	if (err==0)
+          		$display("***TEST PASSED! :) ***");
+        	$finish;
+      	end
+
+    traffic_lights top (
+	.clk (clk),
+	.red (light[2]),
+	.amber (light[1]),
+	.green (light[0])
+     );
+     
+endmodule 
+
+
+	
+
+	
