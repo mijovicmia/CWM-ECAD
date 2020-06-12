0b27e801782b968acf6cac38ef94190ace235355
Cloud User <centos@ip-172-31-10-114.eu-west-2.compute.internal>
2020-06-12 11:11:56 +0000

    In progress

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
-// Date: 
+// Student Name:Mia Mijovic
+// Date: 12/06/2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory and AXI-4-lite interface.
@@ -12,3 +12,52 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top(
+	input clk,
+	input rst,
+	input [2:0] a,
+	input [2:0] b,
+	input read,
+
+	output [5:0] result,
+	output [31:0] rdata,
+	output [31:0] araddr
+	);
+
+	//registers and wires
+	wire rvalid;
+	wire [1:0] rresp;
+	wire aaready;
+	wire [31:0] araddr;
+
+	assign araddr = {26'b0, a, b};
+	assign result = rdata[5:0];
+
+
+blk_mem_gen_0 blk_mem_gen_0 (
+  //.rsta_busy(rsta_busy),          // output wire rsta_busy -- Noa "can write values directly"
+  //.rstb_busy(rstb_busy),          // output wire rstb_busy
+  .s_aclk(clk),                // input wire s_aclk
+  .s_aresetn(rst),          // input wire s_aresetn
+  //.s_axi_awaddr(32'b0),    // input wire [31 : 0] s_axi_awaddr  
+  //.s_axi_awvalid(1'b0),  // input wire s_axi_awvalid
+  //.s_axi_awready(s_axi_awready),  // output wire s_axi_awready
+  //.s_axi_wdata(32'b0),      // input wire [31 : 0] s_axi_wdata
+  //.s_axi_wstrb(4'b0),      // input wire [3 : 0] s_axi_wstrb
+  //.s_axi_wvalid(1'b0),    // input wire s_axi_wvalid
+  //.s_axi_wready(s_axi_wready),    // output wire s_axi_wready
+  //.s_axi_bresp(s_axi_bresp),      // output wire [1 : 0] s_axi_bresp
+  //.s_axi_bvalid(s_axi_bvalid),    // output wire s_axi_bvalid s_a
+
+  .s_axi_araddr(araddr),    // input wire [31 : 0] s_axi_araddr --- {26'b0, a, b}
+  .s_axi_arvalid(1'b1),  // input wire s_axi_arvalid
+  .s_axi_arready(aaready),  // output wire s_axi_arready
+  .s_axi_rdata(rdata),      // output wire [31 : 0] s_axi_rdata ---- 
+  .s_axi_rresp(rresp),      // output wire [1 : 0] s_axi_rresp
+  .s_axi_rvalid(rvalid),    // output wire s_axi_rvalid
+  .s_axi_rready(1'b1)     // input wire s_axi_rready
+);
+
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,69 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8 - Times Table using AXI interface
-// Student Name:
-// Date: 
+// Student Name:Mia Mijovic
+// Date: 12/06/2020
 //
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    	);
+
+	//parameters
+	parameter CLK_PERIOD = 10;
+
+	reg clk;
+	reg read;
+	reg rst;
+	reg err;
+	reg [2:0] a, b;
+	wire[5:0] result;
+	wire [31:0] rdata;
+	wire [31:0] araddr;
+
+	//create CLK
+    	initial begin
+       	clk = 1'b0;
+       		forever
+         	#(CLK_PERIOD/2) clk=~clk;
+    	end
+
+	initial begin
+	err = 0;
+	a = 1;
+	b = 1;
+	read = 1;
+	rst = 0;
+	#CLK_PERIOD
+	rst = 1;
+	#CLK_PERIOD
+		forever begin
+		#CLK_PERIOD
+		$display("result = %b, when a=%b, and b=%b, rdata = %b, araddr=%b", result, a, b, rdata, araddr);
+		if ((a==0&&b==0)&&(result!=0))begin
+			err = 1;
+			$display("*** Test Failed *** result = %b, when a=%b and b=%b", result, a, b);
+		end
+
+		//a = a + 1;
+		
+		end
+
+	end
+
+
+	//Determine success
+      	initial begin
+        	#500
+        	if (err==0)
+          		$display("***TEST PASSED! :) ***");
+        	$finish;
+      	end
+	
+	//instance dice module
+    	top seven_multiplication(clk, rst, a, b, read, result, rdata, araddr);
+
+
+endmodule
